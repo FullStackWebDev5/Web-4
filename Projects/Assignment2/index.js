cc19be2ad1de97106e23705d0cf42be714174549
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2023-04-08 20:57:12 +0530

    Counter App

--- Projects/Assignment2/index.js
+++ Projects/Assignment2/index.js
@@ -1,1 +1,47 @@
-console.log('Hi')
+function increment(input) {
+	var count = Number(document.getElementById('count').innerText)
+	count = count + input
+	document.getElementById('count').innerText = count
+}
+
+function decrement(input) {
+	var count = Number(document.getElementById('count').innerText)
+	count = count - input
+	document.getElementById('count').innerText = count
+}
+
+function incrementRandom() {
+	var input = Number(document.getElementById('increment-input').value)
+	increment(input)
+}
+
+function decrementRandom() {
+	var input = Number(document.getElementById('increment-input').value)
+	decrement(input)
+}
+
+function reset() {
+	document.getElementById('count').innerText = 0
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// count = count + 1; count++; count += 1 
+/*
+	function greet(firstName, lastName) {
+		console.log('Hi, ' + firstName + ' ' + lastName + '!')
+	}
+*/
